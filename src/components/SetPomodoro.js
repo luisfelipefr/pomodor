9549913125134d18f4fe41008bcc4f4aaf5b8725
Luis Felipe <lfghias232@gmail.com>
2021-11-19 00:14:24 -0300

    input

--- src/components/SetPomodoro.js
+++ src/components/SetPomodoro.js
@@ -1,56 +1,87 @@
-import React, { useContext, useState } from 'react'
-import { SettingsContext } from '../context/SettingsContext'
+import React, { useContext, useState } from "react";
+import { SettingsContext } from "../context/SettingsContext";
+
+const numberRegexTest = /^[0-9\b]+$/;
 
 const SetPomodoro = () => {
+  const [newTimer, setNewTimer] = useState({
+    work: '',
+    short: '',
+    long: '',
+    active: "work",
+  });
 
-    const [newTimer, setNewTimer] = useState({
-        work: 0.2,
-        short: 0.1,
-        long: 0.5,
-        active: 'work'
-    })
-
-    const {updateExecute} = useContext(SettingsContext)
-
-    const handleChange = input => {
-        const {name, value} = input.target
-        switch (name) {
-            case 'work':
-                setNewTimer({
-                    ...newTimer,
-                    work: parseInt(value)
-                })
-                break;
-            case 'shortBreak':
-                setNewTimer({
-                    ...newTimer,
-                    short: parseInt(value)
-                })
-                break;
-            case 'longBreak':
-                setNewTimer({
-                    ...newTimer,
-                    long: parseInt(value)
-                })
-                break;
-        }
+  const { updateExecute } = useContext(SettingsContext);
+  
+  const handleChange = (input) => {
+    const { name, value } = input.target;
+    
+    
+    if (value === '' || !numberRegexTest.test(value)) {
+      input.target.value = "0";
+      console.log(value);
+      return;
     }
-    const handleSubmit = e => {
-        e.preventDefault()
-        updateExecute(newTimer)
+    console.log(value);
+    if(value.length > 3) return;
+    switch (name) {
+      case "work":
+        setNewTimer({
+          ...newTimer,
+          work: parseInt(value),
+        });
+        break;
+      case "shortBreak":
+        setNewTimer({
+          ...newTimer,
+          short: parseInt(value),
+        });
+        break;
+      case "longBreak":
+        setNewTimer({
+          ...newTimer,
+          long: parseInt(value),
+        });
+        break;
     }
-    return (
-        <div className="form-container">
-            <form noValidate onSubmit={handleSubmit}>
-                <div className="input-wrapper">
-                    <input className="input" type="number" name="work" onChange={handleChange} value={newTimer.work} />
-                    <input className="input" type="number" name="shortBreak" onChange={handleChange} value={newTimer.short} />
-                    <input className="input" type="number" name="longBreak" onChange={handleChange} value={newTimer.long} />
-                </div>
-                <button type='submit'>Set Timer</button>
-            </form>
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateExecute(newTimer);
+  };
+  
+  return (
+    <div className="form-container">
+      <form noValidate onSubmit={handleSubmit}>
+        <div className="input-wrapper">
+          <input
+            className="input"
+            type="text"
+            
+            name="work"
+            onChange={handleChange}
+            value={newTimer.work}
+          />
+          <input
+            className="input"
+            type="text"
+            name="shortBreak"
+            onChange={handleChange}
+            value={newTimer.short}
+          />
+          <input
+            className="input"
+            type="text"
+            name="longBreak"
+            onChange={handleChange}
+            value={newTimer.long}
+          />
         </div>
-    )
-}
+        <button type="submit">Set Timer</button>
+      </form>
+    </div>
+  );
+  
+};
 
-export default SetPomodoro
+export default SetPomodoro;
